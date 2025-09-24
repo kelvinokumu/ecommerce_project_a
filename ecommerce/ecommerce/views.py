3e5ac41a11aa7b824edb9ddc90f8e6589b9bae03
kelvin okumu <105194367+kelvinokumu@users.noreply.github.com>
2025-09-24 16:10:28 +0300

    using variables in html

--- ecommerce/ecommerce/views.py
+++ ecommerce/ecommerce/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request,"home.html")
+    context = {
+        # from the db fetch user name
+        "page_title":"Home Page",
+        "user_name":"Kelvin"
+    }
+    return render(request,"home.html", context)
 
 def about(request):
+    
     return render(request,"about.html")
